fdb5046c066209596f37658391028eac6b378075
Csanad Kormos <csanadsuli19@gmail.com>
2022-09-15 03:51:57 +0200

    Feladat16 feladat17

---
+++ src/Feladat17.java
@@ -0,0 +1,12 @@
+public class Feladat17 {
+    public static void main(String[] args) {
+        double[] tomb1 = new double[5];
+        double[] tomb2 = new double[5];
+        double[] tomb3 = new double[5];
+
+        for (int i = 0; i<tomb1.length; i++){
+            tomb3[i] = (tomb1[i] + tomb2[i]);
+        }
+
+    }
+}
