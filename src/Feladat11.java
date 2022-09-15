3077986e1f0d87df09eb19acabbb811e03c1fbed
Csanad Kormos <csanadsuli19@gmail.com>
2022-09-15 02:07:26 +0200

    feladat11 páratlan szamok

---
+++ src/Feladat11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Feladat11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy pozitív egész számot");
+        int bekert = sc.nextInt();
+        int szamlalo = 0;
+        for(int i = 1; i <= bekert; i+=2){
+            szamlalo += i;
+        }
+        System.out.printf("A megadott számnál nem nagyobb páratlan, pozitív, egész számok összege: %d", szamlalo);
+    }
+}
